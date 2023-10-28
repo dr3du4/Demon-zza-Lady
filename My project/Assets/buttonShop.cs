9b8641c944930e4e359c362d8cee52ed5a14f741
dr3du4 <jan.krzyszkowski2@gmail.com>
2023-10-29 00:04:08 +0200

    shop

---
+++ My project/Assets/buttonShop.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class buttonShop : MonoBehaviour
+{
+    public TextMeshProUGUI text1; // Przypisz obiekt TMP Text w inspektorze Unity
+    public TextMeshProUGUI text2; // Przypisz obiekt TMP Text w inspektorze Unity
+    public TextMeshProUGUI text3; // Przypisz obiekt TMP Text w inspektorze Unity
+    public Image icon;
+    public string name;
+    public string soul;
+    public Sprite newSprite; 
+    public string price;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        text1.text = name;
+        text2.text = "Potrzebne Gnieciuchy: "+soul;
+        text3.text = "Cena: "+price;
+        icon.sprite = newSprite;
+    }
+
+}
